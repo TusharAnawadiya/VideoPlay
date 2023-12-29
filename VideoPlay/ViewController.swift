9e564fef4971a20d613fb4b8abb8f187bfb28b04
TusharAnawadiya <tusharanawadiya444@gmail.com>
2023-12-29 11:26:20 +0530

    Initial Commit

---
+++ VideoPlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlay
+//
+//  Created by Arti on 29/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
